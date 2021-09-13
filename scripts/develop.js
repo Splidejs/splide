178e429ec2faf0a21440101c3636b504f5ca8ba3
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-09-13 20:53:05 +0900

    Improve the drag handling.

--- scripts/develop.js
+++ scripts/develop.js
@@ -1,8 +1,16 @@
 const chokidar = require( 'chokidar' );
 const { buildJs } = require( './build-script' );
+const { buildCss } = require( './build-css' );
 
 chokidar.watch( [ './src/js/**/*.ts', '!*.test.ts' ] ).on( 'change', async () => {
-  console.log( 'Building...' );
+  console.log( 'Building Script...' );
   await buildJs()
   console.log( 'Finished' );
 } );
+
+chokidar.watch( [ './src/css/**/*.scss' ] ).on( 'change', async () => {
+  console.log( 'Building CSS...' );
+  await buildCss()
+  console.log( 'Finished' );
+} );
+
