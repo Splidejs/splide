6f34481eec6271b3b38c973230fe3030d081e8f6
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-11-16 13:00:05 +0900

    Add missing files.

---
+++ dist/types/constants/listener-options.d.ts
@@ -0,0 +1,10 @@
+/**
+ * AddEventListenerOptions for listeners that may prevent the browser scroll.
+ *
+ * @since 3.4.1
+ */
+export declare const SCROLL_LISTENER_OPTIONS: {
+    passive: boolean;
+    capture: boolean;
+};
+//# sourceMappingURL=../../../src/js/constants/listener-options.d.ts.map
