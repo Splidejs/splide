accdacb367450603fff1b3d8499ade8d60f4e2d8
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-11-30 10:35:17 +0900

    Adjust the def order.

--- dist/types/types/index.d.ts
+++ dist/types/types/index.d.ts
@@ -1,9 +1,9 @@
-export { Components, Options, ResponsiveOptions, EventMap } from '@splidejs/splide';
-export * from './general';
 /**
  * Explicitly declares the module for extensions.
  *
  * @since 3.6.7
  */
 declare module '@splidejs/splide' { }
+export * from '@splidejs/splide';
+export * from './general';
 //# sourceMappingURL=../../../src/js/types/index.d.ts.map
