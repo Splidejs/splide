e298cee1087ce1b382c9e3da942799cd1c03b0c8
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-09-20 18:23:43 +0900

    Merge changes of v4.1.0.

--- dist/js/splide.cjs.js
+++ dist/js/splide.cjs.js
@@ -1,6 +1,6 @@
 /*!
  * Splide.js
- * Version  : 4.0.20
+ * Version  : 4.1.0
  * License  : MIT
  * Copyright: 2022 Naotoshi Fujita
  */
--- dist/js/splide.js
+++ dist/js/splide.js
@@ -4,7 +4,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 /*!
  * Splide.js
- * Version  : 4.0.20
+ * Version  : 4.1.0
  * License  : MIT
  * Copyright: 2022 Naotoshi Fujita
  */
