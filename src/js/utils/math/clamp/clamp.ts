780aac41c2a0ab293c515666de4bac0c1fbd87f2
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-09-20 17:12:23 +0900

    Add a jsdoc `return`.

--- src/js/utils/math/clamp/clamp.ts
+++ src/js/utils/math/clamp/clamp.ts
@@ -7,6 +7,8 @@ import { max, min } from '../math/math';
  * @param number - A subject number to check.
  * @param x      - A min or max number.
  * @param y      - A min or max number.
+ *
+ * @return A clamped number.
  */
 export function clamp( number: number, x: number, y: number ): number {
   const minimum = min( x, y );
