68a076706fc1bfe78768683132b62657b9ae7be2
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-09-20 13:20:52 +0900

    Add a comment.

--- src/js/constants/media.ts
+++ src/js/constants/media.ts
@@ -1,1 +1,6 @@
+/**
+ * Matches when users request reducing non-essential animations.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
+ */
 export const MEDIA_PREFERS_REDUCED_MOTION = '(prefers-reduced-motion: reduce)';
