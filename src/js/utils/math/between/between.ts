0f05990b11c9330928c58dde59063df9c156dbaf
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-09-20 17:11:49 +0900

    Reformat code.

--- src/js/utils/math/between/between.ts
+++ src/js/utils/math/between/between.ts
@@ -2,15 +2,17 @@ import { max, min } from '../math/math';
 
 
 /**
- * Checks if the subject number is between `minOrMax` and `maxOrMin`.
+ * Checks if the subject number is between `x` and `y`.
  *
  * @param number    - A subject number to check.
- * @param minOrMax  - A min or max number.
- * @param maxOrMin  - A max or min number.
+ * @param x         - A min or max number.
+ * @param y         - A max or min number.
  * @param exclusive - Optional. Whether to exclude `x` or `y`.
  */
-export function between( number: number, minOrMax: number, maxOrMin: number, exclusive?: boolean ): boolean {
-  const minimum = min( minOrMax, maxOrMin );
-  const maximum = max( minOrMax, maxOrMin );
-  return exclusive ? minimum < number && number < maximum : minimum <= number && number <= maximum;
+export function between( number: number, x: number, y: number, exclusive?: boolean ): boolean {
+  const minimum = min( x, y );
+  const maximum = max( x, y );
+  return exclusive
+    ? minimum < number && number < maximum
+    : minimum <= number && number <= maximum;
 }
