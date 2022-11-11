f3f1b70b37a5106cd939916b5d2e41ebd24fc943
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-11-11 13:13:54 +0900

    Add the type guard with `asserts`.

--- src/js/utils/error/assert/assert.ts
+++ src/js/utils/error/assert/assert.ts
@@ -7,7 +7,7 @@ import { PROJECT_CODE } from '../../../constants/project';
  * @param condition - If falsy, an error is thrown.
  * @param message   - Optional. A message to display.
  */
-export function assert(condition: any, message?: string): void {
+export function assert<T>(condition: T, message?: string): asserts condition is NonNullable<T> {
   if (!condition) {
     throw new Error(`[${ PROJECT_CODE }] ${ message || '' }`);
   }
