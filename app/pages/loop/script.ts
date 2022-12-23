c00cd20c8ad7d759bc64949ab3a903a778001b84
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-12-23 14:06:54 +0900

    Add "loop" test files. Enable `rewind` in the `RTL` carousel.

--- app/pages/loop/script.ts
+++ app/pages/loop/script.ts
@@ -1,6 +1,7 @@
 import { Splide } from '../../../src/js';
 import { insertHtml } from '../../js/html';
 import '../../js/common';
+import './styles.scss';
 
 
 insertHtml();
@@ -9,11 +10,9 @@ new Splide('#splide01', {
   type: 'loop',
   width: 1000,
   height: 400,
+  speed: 1000,
   perPage: 3,
   gap: 5,
-  direction: 'rtl',
-  padding: {
-    left: 40,
-    right: 0,
-  },
+  updateOnMove: true,
+  // padding: '3rem',
 }).mount();
